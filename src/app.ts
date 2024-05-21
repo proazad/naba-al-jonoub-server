78e7b0eccf712ddedb0b2eb4da2aa6f277ff2f7f
proazad <azadhossain7595.com>
2024-05-21 08:54:21 +0300

    Deployed at vercel

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
+import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
+
+// Parser
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello, I'm from Naba Al Junoub");
 });
